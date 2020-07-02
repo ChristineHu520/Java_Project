b18f070dd8957395af836a6d4e939dacfced7419
ChristineHu520 <1760812359@qq.com>
2020-07-02 09:01:20 +0800

    add 17

--- FirstJava/src/com/thread/demo17_File/demo17_File2.java
+++ FirstJava/src/com/thread/demo17_File/demo17_File2.java
@@ -1,8 +1,25 @@
 package com.thread.demo17_File;
 
+import java.io.File;
+import java.util.logging.FileHandler;
+
 /**
  * @author ChristineHu
  * @date 2020-07-01 8:51
  */
 public class demo17_File2 {
+    public static void main(String[] args) {
+        show1();
+    }
+
+    public static void show1(){
+        File f1 = new File("D:\\HundSun\\bop2.2");
+        System.out.println(f1);  //绝对路径以文件夹结尾，D:\HundSun\bop2.2,以文件夹结尾
+
+        File f2 = new File("D:\\HundSun\\bop2.2\\b.txt");
+        System.out.println(f2);  //绝对路径以文件结尾，D:\HundSun\bop2.2\b.txt
+
+        File f3 = new File("b.txt");
+        System.out.println(f3);  //相对路径，b.txt
+    }
 }

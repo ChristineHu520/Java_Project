bb9becf1f402082b0595dd2501c49b7a59c62bc3
ChristineHu520 <1760812359@qq.com>
2020-08-26 08:56:59 +0800

    add socket2

--- FirstJava/src/com/tcp/demo1_TcpClient/TCPServer.java
+++ FirstJava/src/com/tcp/demo1_TcpClient/TCPServer.java
@@ -3,21 +3,24 @@ package com.tcp.demo1_TcpClient;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 /**
  * @author ChristineHu
- * @date 2020-08-26 8:55
+ * @date 2020-08-26 8:43
  */
-public class TCPClient {
+public class TCPServer {
     public static void main(String[] args) throws IOException {
-        Socket socket = new Socket("127.0.0.1",8888);
-        OutputStream outputStream = socket.getOutputStream();
-        outputStream.write("你好，服务器".getBytes());
+        ServerSocket serverSocket = new ServerSocket(8888);
+        Socket socket = serverSocket.accept();
         InputStream inputStream = socket.getInputStream();
         byte[] bytes = new byte[1024];
         int len = inputStream.read(bytes);
-        System.out.println(new java.lang.String(bytes,0,len));
+        System.out.println(new String(bytes,0,len));
+        OutputStream outputStream = socket.getOutputStream();
+        outputStream.write("服务器收到！".getBytes());
         socket.close();
+        serverSocket.close();
     }
 }

57d0156e4e1de2e8515fa974cf4ef6f08e312d7c
ChristineHu520 <1760812359@qq.com>
2020-09-01 08:54:03 +0800

    add fileupload2

--- FirstJava/src/com/tcp/demo2_FileUpload/FileServer.java
+++ FirstJava/src/com/tcp/demo2_FileUpload/FileServer.java
@@ -3,6 +3,7 @@ package com.tcp.demo2_FileUpload;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Random;
 
 /**
  * @author ChristineHu
@@ -11,21 +12,43 @@ import java.net.Socket;
 public class FileServer {
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(8888);
-        Socket socket = serverSocket.accept();
-        InputStream inputStream = socket.getInputStream();
-        File file = new File("E:\\Users\\husj27225\\JavaProjects\\FirstJava\\src\\com\\tcp\\demo2_FileUpload\\upload");
-        if(!file.exists())
-            file.mkdirs();
-        FileOutputStream fileOutputStream = new FileOutputStream(file + "\\file.txt");
-        byte[] bytes = new byte[1024];
-        int len = 0;
-        while ((len = inputStream.read(bytes))!=-1){
-            fileOutputStream.write(bytes,0,len);
+        //让服务器一直处于监听状态
+        while (true){
+            Socket socket = serverSocket.accept();
+            /*
+                开启多线程，有一个客户端发送请求就开启一个线程
+             */
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        InputStream inputStream = socket.getInputStream();
+                        File file = new File("E:\\Users\\husj27225\\JavaProjects\\FirstJava\\src\\com\\tcp\\demo2_FileUpload\\upload");
+                        if(!file.exists())
+                            file.mkdirs();
+                        //自定义文件名称，域名+毫秒值
+                        String fileName = "itcast" + System.currentTimeMillis() + new Random().nextInt(999999) + ".txt";
+                        FileOutputStream fileOutputStream = new FileOutputStream(file + "\\" + fileName);
+                        byte[] bytes = new byte[1024];
+                        int len = 0;
+                        while ((len = inputStream.read(bytes))!=-1){
+                            fileOutputStream.write(bytes,0,len);
+                        }
+                        OutputStream outputStream = socket.getOutputStream();
+                        outputStream.write("上传成功".getBytes());
+                        fileOutputStream.close();
+                    }catch (IOException e){
+                        System.out.println(e);
+                    }finally {
+                        try {
+                            socket.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }).start();
+            //serverSocket.close();
         }
-        OutputStream outputStream = socket.getOutputStream();
-        outputStream.write("上传成功".getBytes());
-        fileOutputStream.close();
-        socket.close();
-        serverSocket.close();
     }
 }

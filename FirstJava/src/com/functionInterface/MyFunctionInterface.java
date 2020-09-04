f5ee2c6d21c4e23f23eae6962892a952cc3b799f
ChristineHu520 <1760812359@qq.com>
2020-09-04 09:01:53 +0800

    add functioninterface

---
+++ FirstJava/src/com/functionInterface/MyFunctionInterface.java
@@ -0,0 +1,11 @@
+package com.functionInterface;
+
+/**
+ * @author ChristineHu
+ * @date 2020-09-04 8:57
+ */
+@FunctionalInterface
+public interface MyFunctionInterface {
+    public abstract void method();
+    //void method2();
+}
---
+++ FirstJava/src/com/functionInterface/MyFunctionInterfaceImpl.java
@@ -0,0 +1,12 @@
+package com.functionInterface;
+
+/**
+ * @author ChristineHu
+ * @date 2020-09-04 8:55
+ */
+
+public class MyFunctionInterfaceImpl {
+    public static void main(String[] args) {
+
+    }
+}

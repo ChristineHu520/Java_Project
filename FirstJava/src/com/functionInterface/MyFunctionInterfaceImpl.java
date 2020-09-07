2fd64ac344601d79b113a877e2afaa3206bf03aa
ChristineHu520 <1760812359@qq.com>
2020-09-07 08:49:18 +0800

    add functioninterface

--- FirstJava/src/com/functionInterface/MyFunctionInterfaceImpl.java
+++ FirstJava/src/com/functionInterface/MyFunctionInterfaceImpl.java
@@ -5,8 +5,9 @@ package com.functionInterface;
  * @date 2020-09-04 8:55
  */
 
-public class MyFunctionInterfaceImpl {
-    public static void main(String[] args) {
-
+public class MyFunctionInterfaceImpl implements MyFunctionInterface{
+    @Override
+    public void method() {
+        System.out.println("函数式接口的实现类对对象");
     }
 }
---
+++ FirstJava/src/com/functionInterface/demo1_FunctionInterface.java
@@ -0,0 +1,31 @@
+package com.functionInterface;
+
+import java.lang.reflect.Method;
+
+/**
+ * @author ChristineHu
+ * @date 2020-09-07 8:35
+ */
+public class demo1_FunctionInterface{
+    /*
+    接口的入参是一个函数式接口
+     */
+    private static void show1(MyFunctionInterface myFunctionInterface) {
+        myFunctionInterface.method();
+    }
+    public static void main(String[] args) {
+        //传递实现类对象
+        show1(new MyFunctionInterfaceImpl());
+        //传递匿名内部类
+        show1(new MyFunctionInterface() {
+            @Override
+            public void method() {
+                System.out.println("函数式接口的匿名内部类");
+            }
+        });
+        //使用lambda表达式
+        show1(()->{
+            System.out.println("函数式接口的ambda表达式");
+        });
+    }
+}
